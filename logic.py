69f654bd1fae43b3f71c1c608127fad96b5ff5d0
IllyaOnishchenko <70971808+IllyaOnishchenko@users.noreply.github.com>
2020-09-16 17:02:00 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,18 +1,6 @@
-#def numbers():
-	#nums = [45, 55, 60, 37, 100, 105, 220]
-	#if nums / n:
-	#	print (nums)
-def kil(n):
+def numbers():
+	nums = [45, 55, 60, 37, 100, 105, 220]
+	if nums / n:
+		print (nums)
 
-
-    u=0
-    nums = [45, 55, 60, 37, 100, 105, 220]
-
-
-    for i in range(len(nums)-1):
-        if ((n != 0) and ((nums[i] % int(n)) == 0)) :
-            	print(nums[i])
-            	u=1
-            
-    if u == 0 :
-        print ("Zbs...")
+	
