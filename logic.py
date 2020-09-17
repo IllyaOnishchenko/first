bd8e3a69160437fa4c6460169107738a12a0d40e
IllyaOnishchenko <70971808+IllyaOnishchenko@users.noreply.github.com>
2020-09-17 08:29:54 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,5 +1,10 @@
-def numbers():
+def numbers(n):
+	
+
 	nums = [45, 55, 60, 37, 100, 105, 220]
+
 	for value in nums:
 		if value % n == 0:
 			print(value)
+		else:
+			print('Ne delitsa, vvedite drygoe chislo')
